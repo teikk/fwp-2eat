219b2f118c9101592dd0a5e0cd00cf406e12e02c
teik <konrad.l@fabrykawp.pl>
2017-04-11 12:54:42 +0200

    customize acf path and include google maps

--- fwp-2eat.php
+++ fwp-2eat.php
@@ -12,7 +12,7 @@ Author URI: http://fabrykawp.pl/
 
 define( 'FWPR_DIR' , plugin_dir_path(__FILE__) );
 define('FWPR_URI', plugin_dir_url( __FILE__ ));
-
+define('FWPR_DEV',true);
 
 function fwpr_template($slug,$name='',$data = ''){
 	if(!empty($name)){
@@ -33,10 +33,13 @@ function fwpr_parse($string){
 }
 
 add_action('plugins_loaded',array('FWPR_Food','init'));
+
 class FWPR_Food {
 	protected static $instance;
 	public $test = __CLASS__;
-	function __construct(){}
+	public $payment_types = array();
+	function __construct(){	
+	}
 
 	public static function get_instance() {
 		// create an object
@@ -49,11 +52,13 @@ class FWPR_Food {
 		$instance = self::get_instance();
 		add_action( 'after_setup_theme', array($instance,'theme_setup') );
 		add_action( 'wp_enqueue_scripts', array($instance,'scripts') );
+
 	}
 	public function scripts(){
 		wp_register_script( 'fwpr-plugins', FWPR_URI . 'assets/plugins.js', array( 'jquery' ), false, true );
 		wp_register_script( 'fwpr-app', FWPR_URI . 'assets/app.js', array( 'jquery' ), false, true );
 		wp_register_script( 'fwpr-bd', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/js/bootstrap-datepicker.min.js', array( 'jquery' ), false, true );
+		wp_register_script( 'fwpr-gmap','https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyCKMkFobR97jdlA_UyomSRNWQH8cK1E7zQ&libraries=places,drawing,geometry', array( 'jquery' ), '1.0', false );
 		wp_enqueue_script( 'fwpr-plugins' );
 		wp_enqueue_script( 'fwpr-bd' );
 		wp_enqueue_script( 'fwpr-app' );
@@ -70,8 +75,30 @@ class FWPR_Food {
 			add_theme_support( 'post-thumbnails' );
 		}
 	}
-		
+
 }
 
+// 1. customize ACF path
+add_filter('acf/settings/path', 'fwpr_acf_path');
+ 
+function fwpr_acf_path( $path ) {
+	// update path
+	$path = FWPR_DIR . 'acf/';
+	// return
+	return $path;
+}
+ 
+
+// 2. customize ACF dir
+add_filter('acf/settings/dir', 'fwpr_acf_dir');
+ 
+function fwpr_acf_dir( $dir ) { 
+	// update path
+	$dir = FWPR_URI . 'acf/';
+	// return
+	return $dir;	 
+}
+// add_filter('acf/settings/show_admin', '__return_false');
 
 require_once( FWPR_DIR . 'load.php' );
+
