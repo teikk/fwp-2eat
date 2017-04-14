d784c001d5209df5c120fcc4051ddbbaa94991d9
teik <konrad.l@fabrykawp.pl>
2017-04-14 09:57:27 +0200

    add support for input type number min and max value

--- options-template/global-fields.php
+++ options-template/global-fields.php
@@ -1,7 +1,18 @@
 <?php 
 	$name = "fwpr_{$args['group']}_options[{$args['name']}]";
 	$id = $args['label_for'];
-	$value = $this->options[$args['group']][$args['name']];
+	if( isset( $this->options[$args['group']][$args['name']] ) ) {
+		$value = $this->options[$args['group']][$args['name']];
+	} else {
+		$value = '';
+	}
 	$value = htmlspecialchars($value);
+	if( $args['type'] == 'number' ) {
+		$min = 'min="'.$args['min'].'"';
+		$max = 'max="'.$args['max'].'"';
+	} else {
+		$min = '';
+		$max = '';
+	}
  ?>
-<input id="<?php echo $id; ?>" type="<?php echo $args['type']; ?>" name="<?php echo $name;?>" value="<?php echo $value; ?>">
+<input id="<?php echo $id; ?>" type="<?php echo $args['type']; ?>" name="<?php echo $name;?>" value="<?php echo $value; ?>" <?php echo $min; ?> <?php echo $max; ?>>
