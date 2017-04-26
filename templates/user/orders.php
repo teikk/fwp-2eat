7cf3d2bd1385ce3aa2f8c229514081f7d47e3f6c
teik <konrad.l@fabrykawp.pl>
2017-04-26 12:47:20 +0200

    add user orders templates

--- templates/user/orders.php
+++ templates/user/orders.php
@@ -1,10 +1,10 @@
 <?php 
 $orders = fwpr_getUserOrders();
-
 ?>
-<?php if( $orders->have_posts() ): ?>
-	<?php while( $orders->have_posts() ): $orders->the_post(); ?>
-		<?php the_title();?>
-		<?php echo fwpr_payment_label(get_field('order_payment_type')); ?>
-	<?php endwhile; ?>
+<?php if( $orders->have_posts() && is_user_logged_in() ): ?>
+	<div class="fwpr-user">
+		<?php while( $orders->have_posts() ): $orders->the_post(); ?>
+			<?php fwpr_template('user/single-order'); ?>
+		<?php endwhile; ?>
+	</div>
 <?php endif; ?>
---
+++ templates/user/single-order.php
@@ -0,0 +1,4 @@
+<div class="fwpr-user__order">
+<?php the_title(); ?>
+<?php echo fwpr_listOrderProducts( get_the_ID() ); ?>
+</div>
